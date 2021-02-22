f25f6ac84945eb65c7c6719bfa9ef492a0c18cf2
raulIxc85 <raul.ixc@gmail.com>
2021-02-22 08:11:53 -0600

    Cambios formulario Estudiante y Catedratico

--- frontend/src/js/common/components/Catedratico/Formulario.js
+++ frontend/src/js/common/components/Catedratico/Formulario.js
@@ -52,102 +52,108 @@ class Formulario extends Component{
         return(
             <form onSubmit={handleSubmit} >
                 <h3>{titulo}</h3>
-                <div className='w-75'>
-
-                    <label>Nombres</label>
-                    <Field name='perfil.nombres' component={renderField} disabled={disabled}  />
-                    <label>Apellidos</label>
-                    <Field name='perfil.apellidos' component={renderField} disabled={disabled} />
-                    <label>Direccion</label>
-                    <Field name='perfil.direccion' component={renderField} disabled={disabled} />
-                </div>
-                <div className='w-25'>
-                    <label>Telefono</label>
-                    <Field
-                        numberFormat={"+(502) ####-####"}
-                        name='perfil.telefono' 
-                        component={renderNumber} 
-                        disabled={disabled} 
-                    />
-                </div>
-                <div className='w-50'>
-                    <label>Profesión</label>
-                        <Field 
-                            name="profesion" 
-                            placeholder="Seleccionar..." 
-                            component={SelectField} 
-                            options={_listaProfesion} 
-                            className="form-control" 
-                            disabled={disabled}
-                        />
-                </div>
-                <div className='w-25'>
-                    <label>Correo Electronico</label>
-                    <Field 
-                        name='perfil.user.username' 
-                        component={renderField} 
-                        disabled={correo} 
-                    />
-                </div>
-                <div className='row'>
-                    <div className='col-md-6'>
-                        <label>{contra}</label>
-                        <Field 
-                            name='password' 
-                            component={renderField} 
-                            disabled={disabled}
-                            type={hidden}                               
-                        />
-                    </div>
-                    <div className='col-md-6'>
-                        <label>{confContra}</label>
-                        <Field 
-                            name='confirmacion' 
-                            component={renderField} 
-                            disabled={disabled} 
-                            type={hidden}                        
-                        />
-                    </div>   
-                    <div className='col-md-6'>
-                        <label>Género</label>
-                        <Field 
-                            name="perfil.gender"
-                            placeholder="Seleccionar..." 
-                            options={genders} 
-                            component={SelectField} 
-                            className="form-control" 
-                            disabled={disabled}
-                        />
-                    </div>
-                    <div className='col-md-6'>
-                        <label>Rol</label>
-                        <Field 
-                            name="perfil.rol" 
-                            component={SelectField} 
-                            options={rol} 
-                            className="form-control" 
-                            disabled={disabled}
-                        />
-                    </div> 
-                </div>
+                <div className="mb-4 card card-small">
+                    <div className="p-0 pt-3 d-flex flex-column flex-md-row">
+                        <div className="d-flex flex-column flex-1 mx-3">
                 
-                <br />
-                <div className='d-flex flex-row justify-content-end mt-3'> 
-                    <a
-                        href='/#/catedraticos'
-                        className='btn btn-secondary btn-sm mr-2'
-                    >
-                        Cancelar
-                    </a>
-                    
-                    { disabled == false &&
-                        <button
-                            className={`btn btn-sm ${editar ? 'btn-success' : 'btn-primary'}`}
-                            type='submit'
-                        >   
-                            Registrar 
-                        </button>
-                    }
+                            <div className='w-75'>
+                                <label>Nombres</label>
+                                <Field name='perfil.nombres' component={renderField} disabled={disabled}  />
+                                <label>Apellidos</label>
+                                <Field name='perfil.apellidos' component={renderField} disabled={disabled} />
+                                <label>Direccion</label>
+                                <Field name='perfil.direccion' component={renderField} disabled={disabled} />
+                            </div>
+                            <div className='w-25'>
+                                <label>Telefono</label>
+                                <Field
+                                    numberFormat={"+(502) ####-####"}
+                                    name='perfil.telefono' 
+                                    component={renderNumber} 
+                                    disabled={disabled} 
+                                />
+                            </div>
+                            <div className='w-50'>
+                                <label>Profesión</label>
+                                    <Field 
+                                        name="profesion" 
+                                        placeholder="Seleccionar..." 
+                                        component={SelectField} 
+                                        options={_listaProfesion} 
+                                        className="form-control" 
+                                        disabled={disabled}
+                                    />
+                            </div>
+                            <div className='w-25'>
+                                <label>Correo Electronico</label>
+                                <Field 
+                                    name='perfil.user.username' 
+                                    component={renderField} 
+                                    disabled={correo} 
+                                />
+                            </div>
+                            <div className='row'>
+                                <div className='col-md-6'>
+                                    <label>{contra}</label>
+                                    <Field 
+                                        name='password' 
+                                        component={renderField} 
+                                        disabled={disabled}
+                                        type={hidden}                               
+                                    />
+                                </div>
+                                <div className='col-md-6'>
+                                    <label>{confContra}</label>
+                                    <Field 
+                                        name='confirmacion' 
+                                        component={renderField} 
+                                        disabled={disabled} 
+                                        type={hidden}                        
+                                    />
+                                </div>   
+                                <div className='col-md-6'>
+                                    <label>Género</label>
+                                    <Field 
+                                        name="perfil.gender"
+                                        placeholder="Seleccionar..." 
+                                        options={genders} 
+                                        component={SelectField} 
+                                        className="form-control" 
+                                        disabled={disabled}
+                                    />
+                                </div>
+                                <div className='col-md-6'>
+                                    <label>Rol</label>
+                                    <Field 
+                                        name="perfil.rol" 
+                                        component={SelectField} 
+                                        options={rol} 
+                                        className="form-control" 
+                                        disabled={disabled}
+                                    />
+                                </div> 
+                            </div>
+                            
+                            <br />
+                            <div className='d-flex flex-row justify-content-end mt-3'> 
+                                <a
+                                    href='/#/catedraticos'
+                                    className='btn btn-secondary btn-sm mr-2 mb-3'
+                                >
+                                    Cancelar
+                                </a>
+                                
+                                { disabled == false &&
+                                    <button
+                                        className={`btn btn-sm mb-3 ${editar ? 'btn-success' : 'btn-primary'}`}
+                                        type='submit'
+                                    >   
+                                        { editar ? 'Modificar' : 'Registrar' } 
+                                    </button>
+                                }
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </form>
         );
--- frontend/src/js/common/components/Estudiante/Formulario.js
+++ frontend/src/js/common/components/Estudiante/Formulario.js
@@ -37,6 +37,7 @@ class Formulario extends Component{
         }else{
             if (editar==true){
                 hidden = 'hidden';
+                carnetBloq = true;
                 contra = '';
                 confContra = '';
             }
@@ -49,108 +50,119 @@ class Formulario extends Component{
         return(
             <form onSubmit={handleSubmit} >
                 <h3>{titulo}</h3>
-                <div className='w-25'>
-                    <label>Carnet</label>
-                    <Field name='carnet' component={renderField} disabled={carnetBloq} />
-                </div>
-                <label>Nombres</label>
-                <Field name='perfil.nombres' component={renderField} disabled={disabled}  />
-                <label>Apellidos</label>
-                <Field name='perfil.apellidos' component={renderField} disabled={disabled} />
-                <label>Direccion</label>
-                <Field name='perfil.direccion' component={renderField} disabled={disabled} />
-                <div className='w-25'>
-                    <label>Telefono</label>
-                    <Field
-                        numberFormat={"+(502) ####-####"}
-                        name='perfil.telefono' 
-                        component={renderNumber} 
-                        disabled={disabled} 
-                    />
-                </div>
-                <label>Nombre Contacto</label>
-                <Field name='nombreContacto' component={renderField} disabled={disabled}  />
-                <label>Direccion de Contacto</label>
-                <Field name='direccionContacto' component={renderField} disabled={disabled} />
-                <div className='row'>
-                    <div className='col-md-6'>
-                        <label>Telefono de Contacto</label>
-                        <Field 
-                            numberFormat={"+(502) ####-####"}
-                            name='telefonoContacto' 
-                            component={renderNumber} 
-                            disabled={disabled}
-                        />
+                <div className="mb-4 card card-small">
+                    <div className="p-0 pt-3 d-flex flex-column flex-md-row">
+                        <div className="d-flex flex-column flex-1 mx-3">
+                            <div className='w-25'>
+                                <label>Carnet</label>
+                                <Field name='carnet' component={renderField} disabled={carnetBloq} />
+                            </div>
+                            <div className='w-75'>
+                                <label>Nombres</label>
+                                <Field name='perfil.nombres' component={renderField} disabled={disabled}  />
+                                <label>Apellidos</label>
+                                <Field name='perfil.apellidos' component={renderField} disabled={disabled} />
+                                <label>Direccion</label>
+                                <Field name='perfil.direccion' component={renderField} disabled={disabled} />
+                            </div>
+                            <div className='w-25'>
+                                <label>Telefono</label>
+                                <Field
+                                    numberFormat={"+(502) ####-####"}
+                                    name='perfil.telefono' 
+                                    component={renderNumber} 
+                                    disabled={disabled} 
+                                />
+                            </div>
+                            <div className='w-75'>
+                                <label>Nombre Contacto</label>
+                                <Field name='nombreContacto' component={renderField} disabled={disabled}  />
+                                <label>Direccion de Contacto</label>
+                                <Field name='direccionContacto' component={renderField} disabled={disabled} />
+                            </div>
+                            <div className='row'>
+                                <div className='col-md-6'>
+                                    <label>Telefono de Contacto</label>
+                                    <Field 
+                                        numberFormat={"+(502) ####-####"}
+                                        name='telefonoContacto' 
+                                        component={renderNumber} 
+                                        disabled={disabled}
+                                    />
+                                </div>
+                                <div className='col-md-6'>
+                                    <label>Correo Electronico</label>
+                                    <Field 
+                                        name='perfil.user.username' 
+                                        component={renderField} 
+                                        disabled={correo} 
+                                    />
+                                </div>
+                                
+                            </div>
+                            <div className='row'>
+                                <div className='col-md-6'>
+                                    <label>{contra}</label>
+                                    <Field 
+                                        name='password' 
+                                        component={renderField} 
+                                        disabled={disabled}
+                                        type={hidden}    
+                                    />
+                                </div>
+                                <div className='col-md-6'>
+                                    <label>{confContra}</label>
+                                    <Field 
+                                        name='confirmacion' 
+                                        component={renderField} 
+                                        disabled={disabled} 
+                                        type={hidden} 
+                                    />
+                                </div>   
+                                <div className='col-md-6'>
+                                    <label>Género</label>
+                                    <Field 
+                                        name="perfil.gender"
+                                        placeholder="Seleccionar..." 
+                                        options={genders} 
+                                        component={SelectField} 
+                                        className="form-control" 
+                                        disabled={disabled}
+                                    />
+                                </div>
+                                <div className='col-md-6'>
+                                    <label>Rol</label>
+                                    <Field 
+                                        name="perfil.rol" 
+                                        placeholder="Seleccionar..." 
+                                        component={SelectField} 
+                                        options={rol} 
+                                        className="form-control" 
+                                        disabled={disabled}
+                                    />
+                                </div> 
+                            </div>
+                            
+                            <br />
+                            <div className='d-flex flex-row justify-content-end mt-3'> 
+                                <a
+                                    href='/#/estudiantes'
+                                    className='btn btn-secondary btn-sm mr-2 mb-3'
+                                >
+                                    Cancelar
+                                </a>
+                                
+                                { disabled == false &&
+                                    <button
+                                        className={`btn btn-sm mb-3 ${editar ? 'btn-success' : 'btn-primary'}`}
+                                        type='submit'
+                                    >   
+                                        { editar ? 'Modificar' : 'Registrar' }
+                                    </button>
+                                }
+                            </div>
+                        </div>
                     </div>
-                    <div className='col-md-6'>
-                        <label>Correo Electronico</label>
-                        <Field 
-                            name='perfil.user.username' 
-                            component={renderField} 
-                            disabled={correo} 
-                        />
-                    </div>
-                    
-                </div>
-                <div className='row'>
-                    <div className='col-md-6'>
-                        <label>{contra}</label>
-                        <Field 
-                            name='password' 
-                            component={renderField} 
-                            disabled={disabled}
-                            type={hidden}    
-                        />
-                    </div>
-                    <div className='col-md-6'>
-                        <label>{confContra}</label>
-                        <Field 
-                            name='confirmacion' 
-                            component={renderField} 
-                            disabled={disabled} 
-                            type={hidden} 
-                        />
-                    </div>   
-                    <div className='col-md-6'>
-                        <label>Género</label>
-                        <Field 
-                            name="perfil.gender"
-                            placeholder="Seleccionar..." 
-                            options={genders} 
-                            component={SelectField} 
-                            className="form-control" 
-                            disabled={disabled}
-                        />
-                    </div>
-                    <div className='col-md-6'>
-                        <label>Rol</label>
-                        <Field 
-                            name="perfil.rol" 
-                            component={SelectField} 
-                            options={rol} 
-                            className="form-control" 
-                            disabled={disabled}
-                        />
-                    </div> 
-                </div>
-                
-                <br />
-                <div className='d-flex flex-row justify-content-end mt-3'> 
-                    <a
-                        href='/#/estudiantes'
-                        className='btn btn-secondary btn-sm mr-2'
-                    >
-                        Cancelar
-                    </a>
-                    
-                    { disabled == false &&
-                        <button
-                            className={`btn btn-sm ${editar ? 'btn-success' : 'btn-primary'}`}
-                            type='submit'
-                        >   
-                            Registrar 
-                        </button>
-                    }
                 </div>
             </form>
         );
@@ -163,7 +175,7 @@ export const matchPassword = (pass, confirm) => validatorFromFunction(value => {
 
 export default reduxForm({
     form: 'estudiante', //identificador unico del formulario
-    validate: (data) => {
+    /*validate: (data) => {
         return validate(data, {
             confirmPassword: combine(
                validators.exists()('Este campo es requerido'),
@@ -172,5 +184,5 @@ export default reduxForm({
             username: validators.exists()('Este campo es requerido'),
             password: validators.exists()('Este campo es requerido'),
         });
-    },
+    },*/
 })(Formulario)
